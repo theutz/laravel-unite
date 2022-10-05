1e789982064c07bf8b2f750a6a56ac9100ec0230
Michael Utz <michael@theutz.com>
2022-10-05 13:53:38 +0300

    Refactor parser

--- src/Concerns/Parser/Parser.php
+++ src/Concerns/Parser/Parser.php
@@ -13,48 +13,61 @@ use Theutz\Unite\Enums\Prefix;
 
 class Parser implements Contract
 {
+    public function parse(string $str): Value
+    {
+        if (count($parts = explode(separator: ' ', string: $str, limit: 2)) !== 2) {
+            throw new ParseException('The quantity and unit must be separated by a space.');
+        }
+
+        [$quantity, $unit] = $parts;
+
+        return new Value(
+            quantity: $this->parseQuantity($quantity),
+            unit: $this->parseUnit($unit)
+        );
+    }
+
+    public function parseQuantity(BigNumber|int|float|string $quantity): BigNumber
+    {
+        try {
+            return BigNumber::of($quantity);
+        } catch (NumberFormatException $e) {
+            throw new InvalidQuantityException($quantity);
+        }
+    }
+
     public function parseUnit(string $unit): Unit
     {
-        $unit = str($unit);
-
-        try {
-            $baseUnit = BaseUnit::from($unit);
-
+        if ($baseUnit = BaseUnit::tryFrom($unit)) {
             return new Unit(prefix: null, baseUnit: $baseUnit);
-        } catch (\ValueError $e) {
-            // The unit given might have an SI prefix
         }
 
-        try {
-            $baseUnit = collect(BaseUnit::cases())
-                ->firstOrFail(fn ($u) => $unit
-                    ->endsWith($u->value));
-            $prefix = Prefix::from($unit->substr(0, $unit->length() - str($baseUnit->value)->length()));
-        } catch (ItemNotFoundException $e) {
-            throw new InvalidBaseUnitException($unit);
-        } catch (\ValueError $e) {
-            throw new InvalidUnitPrefixException($unit);
-        }
+        $baseUnit = $this->extractBaseUnit($unit);
+        $prefix = $this->extractPrefix($unit, $baseUnit);
 
         return new Unit(prefix: $prefix, baseUnit: $baseUnit);
     }
 
-    public function parse(string $str): Value
+    private function extractPrefix(string $unit, BaseUnit $baseUnit)
     {
         try {
-            [$quantity, $unit] = explode(' ', $str, 2);
+            $unit = str($unit);
+            $baseUnit = str($baseUnit->value);
+            $substr = $unit->substr(start: 0, length: $unit->length() - $baseUnit->length());
 
-            return new Value(
-                quantity: BigNumber::of($quantity),
-                unit: $this->parseUnit($unit)
-            );
-        } catch (NumberFormatException $e) {
-            throw new InvalidQuantityException($quantity);
-        } catch (\Exception $e) {
-            if (str($e->getMessage())->test('/^Undefined array key 1$/')) {
-                throw new ParseException("'$str' is not a valid amount.");
-            }
-            throw $e;
+            return Prefix::from($substr);
+        } catch (\ValueError $e) {
+            throw new InvalidUnitPrefixException($unit);
+        }
+    }
+
+    private function extractBaseUnit(string $unit): BaseUnit
+    {
+        try {
+            return collect(BaseUnit::cases())
+                ->firstOrFail(fn ($u) => str($unit)->endsWith($u->value));
+        } catch (ItemNotFoundException $e) {
+            throw new InvalidBaseUnitException($unit);
         }
     }
 }
--- src/Contracts/Parser.php
+++ src/Contracts/Parser.php
@@ -4,10 +4,13 @@ namespace Theutz\Unite\Contracts;
 
 use Theutz\Unite\DTOs\Unit;
 use Theutz\Unite\DTOs\Value;
+use Brick\Math\BigNumber;
 
 interface Parser
 {
     public function parse(string $str): Value;
 
     public function parseUnit(string $unit): Unit;
+
+    public function parseQuantity(BigNumber|int|float|string $quantity): BigNumber;
 }
