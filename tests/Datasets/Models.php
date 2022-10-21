b4665b826f64962d03290d67980dd092b860e6f0
Michael Utz <theutz@users.noreply.github.com>
2022-10-21 11:30:07 +0300

    Use Config File (#9)
    In this PR, we setup the config file to be the source-of-truth, rather
    than a set of YAML files. This has a few distict advantages, namely the
    following:
    
    1. Config files can easily be published for use changes.
    2. Config files can be cached.
    3. Config files can be merge/extended.
    4. The config file offers a central location where values can be
    coordinated.
    5. Config files use simple PHP arrays, allowing for normalization
    strategy.

--- tests/Datasets/Models.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Theutz\Unite\Data\Finder;
-
-$models = collect(app(Finder::class)->find())
-    ->mapWithKeys(fn ($className) => [class_basename($className) => app($className)]);
-
-dataset('models', $models);
