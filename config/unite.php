30c53b31aa47ac603b7860cbdbec31b14d0b5875
Michael Utz <michael@theutz.com>
2022-10-25 10:23:01 +0300

    Setup config for i18n

--- config/unite.php
+++ config/unite.php
@@ -68,59 +68,77 @@ return [
     ],
 
     /**
-     * A list of units and their corresponding singular names
+     * This is the canonical listing of units.
+     *
+     * The key represents the ID used thorughout the system to idenfity the unit.
+     *
+     * The value represents any long names and/or aliases that might be used to identify
+     * the unit. These will be used to dynamically generate language files for the
+     * `en` locale, and thus the (likely) fallback locale for your app.
+     *
+     * Values are specified with the same syntax that Laravel's localizations values use,
+     * with one signficant exception: aliases can appear separated by two semicolons. In this
+     * way, we can specify multiple representations for a single unit. An example
+     * below:
+     *
+     *    Root name       Alias
+     *    ____|____       __|__
+     *  Sing.    Pl.    Sing. Pl.
+     * ___|___ ___|___  __| __|__
+     * |      |      |  |  |    |
+     * 'ampere|amperes;;amp|amps'
      */
     'units' => [
-        'A' => 'ampere',
-        'acre' => 'acre',
-        'bar' => 'bar',
-        'C' => 'Celsius',
-        'cd' => 'candela',
-        'ch' => 'chain',
-        'cu ft' => 'cubic feet',
-        'cu in' => 'cubic inch',
-        'cu mi' => 'cubic mile',
-        'cu yd' => 'cubic yard',
-        'd' => 'day',
-        'dry pt' => 'dry pint',
-        'dry qt' => 'dry quart',
-        'F' => 'Fahrenheit',
-        'fl oz' => 'fluid ounce',
-        'ft' => 'foot',
-        'fur' => 'furlong',
-        'g' => 'gram',
-        'gal' => 'gallon',
-        'gil' => 'gill',
-        'h' => 'hour',
-        'in' => 'inch',
-        'K' => 'Kelvin',
-        'L' => 'liter',
-        'lb' => 'pound',
-        'lea' => 'league',
-        'long ton' => 'long ton',
-        'm' => 'meter',
-        'm2' => 'meter squared',
-        'm3' => 'meter cubed',
-        'mi' => 'mile',
-        'min' => 'minute',
-        'mmHg' => 'millimeter of mercury',
-        'N' => 'Newton',
-        'oz' => 'ounce',
-        'pt' => 'pint',
-        'qt' => 'quart',
-        'rad' => 'radian',
-        's' => 'second',
-        'sq ft' => 'square foot',
-        'sq in' => 'square inch',
-        'sq mi' => 'square mile',
-        'sq yd' => 'square yard',
-        'st' => 'stere',
-        'tbsp' => 'tablespoon',
-        'ton' => 'ton',
-        'tsp' => 'teaspoon',
-        'V' => 'volt',
-        'W' => 'watt',
-        'yd' => 'yard',
+        'A' => 'ampere|amperes;;amp|amps',
+        'acre' => 'acre|acres',
+        'bar' => 'bar|bars',
+        'C' => 'Celsius;;degree Celsius|degrees Celsius',
+        'cd' => 'candela|candelas',
+        'ch' => 'chain|chains',
+        'cu ft' => 'cubic feet|cubic feet;;° C;°C',
+        'cu in' => 'cubic inch|cubic inches',
+        'cu mi' => 'cubic mile|cubic miles',
+        'cu yd' => 'cubic yard|cubid yards',
+        'd' => 'day|days',
+        'dry pt' => 'dry pint|dry pints',
+        'dry qt' => 'dry quart|dry quarts',
+        'F' => 'Fahrenheit;;degree Fahrenheit|degrees Fahrenheit;;° F;;°F',
+        'fl oz' => 'fluid ounce|fluid ounces',
+        'ft' => 'foot|feet',
+        'fur' => 'furlong|furlongs',
+        'g' => 'gram|grams',
+        'gal' => 'gallon|gallons',
+        'gil' => 'gill|gills',
+        'h' => 'hour|hours',
+        'in' => 'inch|inches',
+        'K' => 'Kelvin;;degree Kelvin|degrees Kelvin;;° K;;°K',
+        'L' => 'liter|liters',
+        'lb' => 'pound|pounds;;lbs',
+        'lea' => 'league|leagues',
+        'long ton' => 'long ton|long tons',
+        'm' => 'meter|meters',
+        'm2' => 'meter squared|meters squared;;square meter|square meters;;m²',
+        'm3' => 'meter cubed|meters cubed;;cubic meter|cubic meters;;m³',
+        'mi' => 'mile|miles',
+        'min' => 'minute|minutes',
+        'mmHg' => 'millimeter of mercury|millimeters of mercury',
+        'N' => 'Newton|Newtons',
+        'oz' => 'ounce|ounces',
+        'pt' => 'pint|pints',
+        'qt' => 'quart|quarts',
+        'rad' => 'radian|radians',
+        's' => 'second|seconds',
+        'sq ft' => 'square foot|square feet',
+        'sq in' => 'square inch|square inches',
+        'sq mi' => 'square mile|square miles',
+        'sq yd' => 'square yard|square yards',
+        'st' => 'stere|steres',
+        'tbsp' => 'tablespoon|tablespoons',
+        'ton' => 'ton|tons',
+        'tsp' => 'teaspoon|teaspoons',
+        'V' => 'volt|volts',
+        'W' => 'watt|watts',
+        'yd' => 'yard|yards',
     ],
 
     /**
