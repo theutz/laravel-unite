a2aed105afe9ebbbe4aabfa96a33ba3ebc116c9e
Michael Utz <michael@theutz.com>
2022-10-31 13:15:56 +0300

    Read config from published files

--- src/UniteServiceProvider.php
+++ src/UniteServiceProvider.php
@@ -24,13 +24,38 @@ class UniteServiceProvider extends PackageServiceProvider
          */
         $package
             ->name('laravel-unite')
-            ->hasConfigFile(['unite', 'unite-prefixes', 'unite-units'])
+            ->hasConfigFile('unite')
             ->hasTranslations();
+        $this->configureDefinitions();
     }
 
     public function packageBooted()
     {
         $this->registerPrivatePackageCommands();
+    }
+
+    private function configureDefinitions()
+    {
+        $definintions = collect([
+            __DIR__ . '/../config/unite-prefixes.php' => 'unite-prefixes',
+            __DIR__ . '/../config/unite-units.php' => 'unite-units',
+        ]);
+
+        // Register files for publishing
+        $publishes = $definintions
+            ->map(fn ($configKey) => config_path("{$configKey}.php"))->all();
+
+        $this->publishes($publishes, 'unite-config');
+
+        // Read from published files instead of package files, if they've been
+        // published. This is in contrast to the more default behavior
+        // of merging values in.
+        $definintions
+            ->each(function ($configKey, $configPath) {
+                if (!file_exists(config_path($configKey . '.php'))) {
+                    config([$configKey => require $configPath]);
+                }
+            });
     }
 
     private function registerPrivatePackageCommands(): void
