90f0429f2ae32bd4531f3b7fd6104b4e24cfa60c
Michael Utz <michael@theutz.com>
2022-10-31 12:14:15 +0300

    Fix unit shape

--- config/unite-units.php
+++ config/unite-units.php
@@ -8,9 +8,10 @@ return [
         'kind' => 'mass',
         'systems' => ['si'],
         'to' => [
-
-            'symbol' => 'oz',
-            'factor' => '0.0352739907',
+            [
+                'symbol' => 'oz',
+                'factor' => '0.0352739907',
+            ],
         ],
     ],
     [
@@ -28,9 +29,10 @@ return [
         'kind' => 'mass',
         'systems' => ['us', 'uk'],
         'to' => [
-
-            'symbol' => 'g',
-            'factor' => '2.83495e1',
+            [
+                'symbol' => 'g',
+                'factor' => '2.83495e1',
+            ],
         ],
     ],
     [
