c37c4bbee36e8d9069090329e12b71a32f122ffe
Michael Utz <michael@theutz.com>
2022-11-03 12:48:14 +0300

    Feature test that defines the problem

--- tests/Feature/UniteTest.php
+++ tests/Feature/UniteTest.php
@@ -11,4 +11,5 @@ it('can convert', function ($from, $to, $expected) {
     ['200 g', 'oz', '7.05479814 oz'],
     ['50 oz', 'g', '1417.475 g'],
     ['50 ounces', 'grams', '1417.475 grams'],
+    ['20 km', 'm', '20000 m'],
 ]);
