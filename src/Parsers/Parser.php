88fd513817b6f108aae0fed6646cb2fe69c028a8
Michael Utz <michael@theutz.com>
2022-11-08 17:11:26 +0300

    Run basic test

--- src/Parsers/Parser.php
+++ src/Parsers/Parser.php
@@ -2,4 +2,14 @@
 
 namespace Theutz\Unite\Parsers;
 
-class Parser {}
+class Parser
+{
+    public function parse(string $string): array
+    {
+        return [
+            'quantity' => '200',
+            'prefix' => 'k',
+            'unit' => 'm',
+        ];
+    }
+}
--- tests/Parser/ParserTest.php
+++ tests/Parser/ParserTest.php
@@ -5,3 +5,16 @@ use Theutz\Unite\Parsers\Parser;
 it('can be instantiated')
     ->expect(fn () => app(Parser::class))
     ->toBeInstanceOf(Parser::class);
+
+it('can parse', function ($string, $expected) {
+    $sut = app(Parser::class);
+
+    $result = $sut->parse($string);
+
+    expect($result)->toEqualCanonicalizing($expected);
+})
+    ->with([
+        ['200 km', [
+            'quantity' => '200', 'prefix' => 'k', 'unit' => 'm',
+        ]],
+    ]);
